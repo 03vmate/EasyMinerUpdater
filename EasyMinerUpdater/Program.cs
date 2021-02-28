636f268a471d6601f942562f6703a47f8a7fc967
03vmate <47510515+03vmate@users.noreply.github.com>
2021-02-28 23:01:25 +0100

    Initial Commit

---
+++ EasyMinerUpdater/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace EasyMinerUpdater
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Error! No args provided.");
+                return;
+            }
+            string path = args[0];
+            Console.WriteLine("Updating, please wait...");
+            System.Threading.Thread.Sleep(5000);
+
+            try
+            {
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://uplexa.online/EasyMiner.exe", path);
+                }
+            }
+            catch (Exception err)
+            {
+                Console.WriteLine("Error updating: " + err.Message);
+                Console.ReadLine();
+            }
+            System.Diagnostics.Process.Start(path);
+        }
+    }
+}
